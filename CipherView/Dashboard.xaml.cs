6072fb9e17a5e967830ce81168c27d4607ccfb6b
brainz <emiliocantoramayo@gmail.com>
2025-10-17 06:46:30 -0400

    Update Dashboard.xaml.cs

--- CipherView/Dashboard.xaml.cs
+++ CipherView/Dashboard.xaml.cs
@@ -1,27 +1,14 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using static CipherView.FetchData;
 using System.Diagnostics;
-using ControlzEx;
+using System.Windows;
+using System.Windows.Input;
+using ControlzEx.Theming;
+using MahApps.Metro.Controls;
 
 namespace CipherView
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
-    public partial class Dashboard
+    public partial class Dashboard : MetroWindow
     {
         public string? ConnectedIpAddress { get; set; }
         public string? WindowStatus { get; set; }
@@ -52,6 +39,22 @@ namespace CipherView
             }
 
             ColorBar = "Green";
+
+            var currentTheme = ThemeManager.Current.DetectTheme(this);
+            if (currentTheme?.Name.Contains("Dark") == true)
+                DarkModeToggle.IsChecked = true;
+        }
+
+        private void DarkModeToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(Application.Current, "Dark.Blue");
+            WindowStatus = "Dark Mode Enabled";
+        }
+
+        private void DarkModeToggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(Application.Current, "Light.Blue");
+            WindowStatus = "Light Mode Enabled";
         }
 
         private void Button_Settings(object sender, RoutedEventArgs e)
@@ -79,7 +82,7 @@ namespace CipherView
 
         private void Button_About(object sender, RoutedEventArgs e)
         {
-            var url = "https://github.com/absolutegoaat";
+            const string url = "https://github.com/absolutegoaat";
             try
             {
                 Process.Start(new ProcessStartInfo
